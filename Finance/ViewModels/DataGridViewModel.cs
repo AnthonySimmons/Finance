d0ba74b5fc73a55baa6be3b763917313f664ed22
Anthony Simmons <Anthony Simmons>
2016-11-24 18:52:12 -0800

    Remove unneeded event handler

--- Finance/ViewModels/DataGridViewModel.cs
+++ Finance/ViewModels/DataGridViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using FinanceModel;
 using FinanceModel.Models;
-using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace Finance.ViewModels
 {
@@ -14,16 +12,7 @@ namespace Finance.ViewModels
         public DataGridViewModel(DataModel dataModel)
         {
             _dataModel = dataModel;
-
-            _dataModel.PropertyChanged += _dataModel_PropertyChanged;
         }
-
-        private void _dataModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            //Transactions.Clear();
-            //Transactions.AddRange(_dataModel.Transactions.OrderBy(t => t.Date));
-        }
-
-        
+                        
     }
 }
